f82dcd60a15ccd006a207b0d31223f0f9cc1ff67
vagrant <vagrant@localhost.localdomain>
2020-10-17 14:03:34 +0000

    note

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    stretch_path
+    notes_path
   end
 
   def after_sign_out_path_for(_resource)
