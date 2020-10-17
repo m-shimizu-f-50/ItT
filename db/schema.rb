e08954c30e8e81d9f4428a399459e3460100f3e0
vagrant <vagrant@localhost.localdomain>
2020-10-17 12:54:40 +0000

    新規登録

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2020_10_13_130002) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
