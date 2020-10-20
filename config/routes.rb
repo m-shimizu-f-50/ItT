6d95bdddb94e4199f58b126dae444849346b8f88
vagrant <vagrant@localhost.localdomain>
2020-10-20 14:35:13 +0000

    削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
-  resources :notes, only: [:new, :create, :index, :show ,:edit, :update]
+  resources :notes, only: [:new, :create, :index, :show ,:edit, :update,:destroy]
 
 end
