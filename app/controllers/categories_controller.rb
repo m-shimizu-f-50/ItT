d8174560e6119c851c72c2931d289e5c12bc4df0
vagrant <vagrant@localhost.localdomain>
2020-10-22 15:44:00 +0000

    category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,19 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
